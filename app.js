0a1d85194e7142e54251efd761ef39ff047cedc3
Sosukeinthematrix <153436101+Sosukeinthematrix@users.noreply.github.com>
2024-09-15 02:46:43 +0900

    Update app.js
    flowise access added

--- app.js
+++ app.js
@@ -10,15 +10,19 @@ app.post('/webhook', async (req, res) => {
 
   try {
     for (let event of events) {
-      // Forward the event to Flowise API
       const response = await axios.post('https://flowise-vy6k.onrender.com/api/v1/prediction', {
         chatflowid: '2f08ed7f-f1db-4d17-9ced-7492b8b7af6d',
         question: event.message.text,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `9hl0o0vux7S_zpIZfSjfGIPTJKSdTdDDS0q-Y8XgYkU`
+        }
       });
 
       console.log(`Flowise response: ${response.data}`);
     }
-    res.status(200).send('OK');  // Respond to LINE with HTTP 200
+    res.status(200).send('OK');
   } catch (error) {
     console.error('Error processing webhook:', error);
     res.status(500).send('Error processing the request');
@@ -29,3 +33,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+});
+
